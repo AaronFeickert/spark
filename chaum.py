c81264e39c26f7861c3c2abc3825f77e89c13a13
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2022-01-21 16:08:19 -0600

    Cleanup: Chaum

--- chaum.py
+++ chaum.py
@@ -6,13 +6,13 @@ import transcript
 class ChaumParameters:
 	def __init__(self,F,G,H,U):
 		if not isinstance(F,Point):
-			raise TypeError('Bad type for parameter F!')
+			raise TypeError
 		if not isinstance(G,Point):
-			raise TypeError('Bad type for parameter G!')
+			raise TypeError
 		if not isinstance(H,Point):
-			raise TypeError('Bad type for parameter H!')
+			raise TypeError
 		if not isinstance(U,Point):
-			raise TypeError('Bad type for parameter U!')
+			raise TypeError
 		
 		self.F = F
 		self.G = G
@@ -22,13 +22,13 @@ class ChaumParameters:
 class ChaumStatement:
 	def __init__(self,params,context,S,T):
 		if not isinstance(params,ChaumParameters):
-			raise TypeError('Bad type for parameters!')
+			raise TypeError
 		if not isinstance(S,PointVector):
-			raise TypeError('Bad type for Chaum statement input S!')
+			raise TypeError
 		if not isinstance(T,PointVector):
-			raise TypeError('Bad type for Chaum statement input T!')
+			raise TypeError
 		if not len(S) == len(T):
-			raise ValueError('Size mismatch for Chaum statement!')
+			raise ValueError
 		
 		self.F = params.F
 		self.G = params.G
@@ -41,13 +41,13 @@ class ChaumStatement:
 class ChaumWitness:
 	def __init__(self,x,y,z):
 		if not isinstance(x,ScalarVector):
-			raise TypeError('Bad type for Chaum witness x!')
+			raise TypeError
 		if not isinstance(y,ScalarVector):
-			raise TypeError('Bad type for Chaum witness y!')
+			raise TypeError
 		if not isinstance(z,ScalarVector):
-			raise TypeError('Bad type for Chaum witness z!')
+			raise TypeError
 		if not len(x) == len(y) and len(y) == len(z):
-			raise ValueError('Size mismatch for Chaum witness!')
+			raise ValueError
 		
 		self.x = x
 		self.y = y
@@ -56,6 +56,7 @@ class ChaumWitness:
 class ChaumProof:
 	def __repr__(self):
 		return repr(hash_to_scalar(
+			'Modified Chaum',
 			self.A1,
 			self.A2,
 			self.t1,
@@ -65,17 +66,17 @@ class ChaumProof:
 
 	def __init__(self,A1,A2,t1,t2,t3):
 		if not isinstance(A1,Point):
-			raise TypeError('Bad type for Chaum proof element A1!')
+			raise TypeError
 		if not isinstance(A2,PointVector):
-			raise TypeError('Bad type for Chaum proof element A2!')
+			raise TypeError
 		if not isinstance(t1,ScalarVector):
-			raise TypeError('Bad type for Chaum proof element t1!')
+			raise TypeError
 		if not isinstance(t2,Scalar):
-			raise TypeError('Bad type for Chaum proof element t2!')
+			raise TypeError
 		if not isinstance(t3,Scalar):
-			raise TypeError('Bad type for Chaum proof element t3!')
+			raise TypeError
 		if not len(A2) == len(t1):
-			raise ValueError('Size mismatch in Chaum proof!')
+			raise ValueError
 
 		self.A1 = A1
 		self.A2 = A2
@@ -85,11 +86,11 @@ class ChaumProof:
 
 def challenge(statement,A1,A2):
 	if not isinstance(statement,ChaumStatement):
-		raise TypeError('Bad type for Chaum statement!')
+		raise TypeError
 	if not isinstance(A1,Point):
-		raise TypeError('Bad type for challenge input A1!')
+		raise TypeError
 	if not isinstance(A2,PointVector):
-		raise TypeError('Bad type for challenge input A2!')
+		raise TypeError
 
 	tr = transcript.Transcript('Modified Chaum')
 	tr.update(statement.F)
@@ -101,22 +102,23 @@ def challenge(statement,A1,A2):
 	tr.update(statement.T)
 	tr.update(A1)
 	tr.update(A2)
+
 	return tr.challenge()
 
 def prove(statement,witness):
 	if not isinstance(statement,ChaumStatement):
-		raise TypeError('Bad type for Chaum statement!')
+		raise TypeError
 	if not isinstance(witness,ChaumWitness):
-		raise TypeError('Bad type for Chaum witness!')
+		raise TypeError
 	
 	n = len(statement.S)
 	
 	# Check the statement validity
 	for i in range(n):
 		if not statement.S[i] == witness.x[i]*statement.F + witness.y[i]*statement.G + witness.z[i]*statement.H:
-			raise ArithmeticError('Invalid Chaum statement!')
+			raise ArithmeticError
 		if not statement.U == witness.x[i]*statement.T[i] + witness.y[i]*statement.G:
-			raise ArithmeticError('Invalid Chaum statement!')
+			raise ArithmeticError
 	
 	r = ScalarVector([random_scalar() for _ in range(n)])
 	s = ScalarVector([random_scalar() for _ in range(n)])
@@ -140,11 +142,11 @@ def prove(statement,witness):
 
 def verify(statement,proof):
 	if not isinstance(statement,ChaumStatement):
-		raise TypeError('Bad type for Chaum statement!')
+		raise TypeError
 	if not isinstance(proof,ChaumProof):
-		raise TypeError('Bad type for Chaum proof!')
+		raise TypeError
 	if not len(proof.A2) == len(statement.S):
-		raise ValueError('Size mismatch in Chaum verification!')
+		raise ValueError
 	
 	n = len(statement.S)
 	
@@ -156,7 +158,7 @@ def verify(statement,proof):
 		L += c**(i+1)*statement.S[i]
 		R += proof.t1[i]*statement.F
 	if not L == R:
-		raise ArithmeticError('Failed Chaum verification!')
+		raise ArithmeticError
 	
 	L = Z
 	R = proof.t2*statement.G
@@ -164,4 +166,4 @@ def verify(statement,proof):
 		L += proof.A2[i] + c**(i+1)*statement.U
 		R += proof.t1[i]*statement.T[i]
 	if not L == R:
-		raise ArithmeticError('Failed Chaum verification!')
+		raise ArithmeticError
